2a221c4805135796245856f94693be9a8c90d2f1
SeyoungCho <wnl383@naver.com>
2024-07-12 00:36:51 +0900

    feat: add model viewer script to ModelViewer component

--- src/components/sections/products/ModelViewer.tsx
+++ src/components/sections/products/ModelViewer.tsx
@@ -3,23 +3,29 @@ import leatherShoes from "@model/vans_shoes.glb?url";
 import iosLeatherShoes from "@model/vans_shoes.usdz?url";
 const ModelViewer = () => {
   return (
-    <div>
-      {/* @ts-ignore*/}
-      <model-viewer
-        class="h-[300px] w-[300px] sm:h-[600px] sm:w-[600px] md:h-[500px] md:w-[500px] lg:h-[800px] lg:w-[800px]"
-        alt="3D Model"
-        src={leatherShoes}
-        ios-src={iosLeatherShoes}
-        ar
-        scale="2 2 2"
-        tone-mapping="neutral"
-        shadow-intensity="1"
-        camera-controls
-        touch-action="pan-y"
-      >
-        {/*@ts-ignore */}
-      </model-viewer>
-    </div>
+    <>
+      <script
+        type="module"
+        src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.5.0/model-viewer.min.js"
+      ></script>
+      <div>
+        {/* @ts-ignore*/}
+        <model-viewer
+          class="h-[300px] w-[300px] sm:h-[600px] sm:w-[600px] md:h-[500px] md:w-[500px] lg:h-[800px] lg:w-[800px]"
+          alt="3D Model"
+          src={leatherShoes}
+          ios-src={iosLeatherShoes}
+          ar
+          scale="2 2 2"
+          tone-mapping="neutral"
+          shadow-intensity="1"
+          camera-controls
+          touch-action="pan-y"
+        >
+          {/*@ts-ignore */}
+        </model-viewer>
+      </div>
+    </>
   );
 };
 
