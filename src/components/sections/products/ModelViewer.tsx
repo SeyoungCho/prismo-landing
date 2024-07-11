fcf15ab07af1bf5e13ffa0be053e00d95f08e7b9
SeyoungCho <wnl383@naver.com>
2024-07-12 00:28:35 +0900

    feat: load model-viewer only client-side

---
+++ src/components/sections/products/ModelViewer.tsx
@@ -0,0 +1,26 @@
+// @ts-nocheck
+import leatherShoes from "@model/vans_shoes.glb?url";
+import iosLeatherShoes from "@model/vans_shoes.usdz?url";
+const ModelViewer = () => {
+  return (
+    <div>
+      {/* @ts-ignore*/}
+      <model-viewer
+        class="h-[300px] w-[300px] sm:h-[600px] sm:w-[600px] md:h-[500px] md:w-[500px] lg:h-[800px] lg:w-[800px]"
+        alt="3D Model"
+        src={leatherShoes}
+        ios-src={iosLeatherShoes}
+        ar
+        scale="2 2 2"
+        tone-mapping="neutral"
+        shadow-intensity="1"
+        camera-controls
+        touch-action="pan-y"
+      >
+        {/*@ts-ignore */}
+      </model-viewer>
+    </div>
+  );
+};
+
+export default ModelViewer;
